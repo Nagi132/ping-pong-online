322f9c15bbdf191914423841d10f75e689a16b5c
Nyseer Couse <nyseer.couse@gmail.com>
2023-10-17 07:14:26 -0400

    titlescreen with play button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
 import TitleScreen from './TitleScreen';
+import Canvas from "./Canvas";
+
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Redirect,
+  Link, 
+} from "react-router-dom"; 
 
 function App() {
+
+  //routing to other pages
+  <Route path="/" element={<App/>}>
+  <Route path="Canvas.js" element={<OpenCanvas/>} />
+  <Route path="TitleScreen.js" element={<TitleScreen/>}/>
+  </Route>;
+
+function OpenCanvas() {
+  // Since Contact is not a parent of EditContact we need to go up one level
+  // in the path, instead of one level in the Route hierarchy
   return (
-    <div className="App">
+    <Link to="Canvas.js" relative="path">
+      Cancel
+    </Link>
+  );
+}
+
+  return (
+     <> 
+      
+
+
+      
+
+      <div className="App">
       <TitleScreen/>
+      <Canvas width="800" height ="500"></Canvas>
+      
     </div>
+    </> 
+   
   );
 }
 
--- src/TitleScreen.js
+++ src/TitleScreen.js
@@ -1,13 +1,44 @@
 import './TitleScreen.css';
+import Canvas from './Canvas';
+
+
+
+function Button () {
+
+  function handleClick() {
+    console.log('You started the game.');
+  }
+
+  return (
+    <div>
+      <button className="button" onClick={handleClick}>Play!</button>
+    </div>
+  );
+}
 
 function TitleScreen () {
   return (
-    <div className="App-background">
-      <header className="App-header">
-        <img src="/images/title-image.png" alt="Game Title" className="title-image" />
-      </header>
+    <>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Title Screen</title>
+        <link rel="stylesheet" type="text/css" href="TitleScreen.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
+
+      </head>
+      <div className="App-background">
+      
+        <div className="App-header">
+          <Button/> 
+        
+          
+        </div>
       </div>
+    </>
   );
 }
+
+
 
 export default TitleScreen;
