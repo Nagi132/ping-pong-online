94bd27d25e71e60668592215a769f8f6f4dd5ec1
Savannah Chhann <schhann327@gmail.com>
2023-10-27 17:21:43 -0400

    added comments for gameplay and canvas functions

--- src/Gameplay.js
+++ src/Gameplay.js
@@ -6,81 +6,96 @@ import './Gameplay.css';
 
 const Gameplay = () => {
 
+    // set up canvas 
     const canvasRef = useRef();
+
+    // frame counter
     const [counter, setCounter] = useState(0);
 
+    // table width and height relative to window dimensions
     const TABLE_WIDTH = window.innerWidth/2 - 500;
     const TABLE_HEIGHT = window.innerHeight/2 - 300;
 
+    // ball x and y positions; vertical and horizontaly speed
     const [ballX, setBallX] = useState(500);
     const [ballY, setBallY] = useState(300);
     const [ballSpeedX, setBallSpeedX] = useState(10);
     const [ballSpeedY, setBallSpeedY] = useState(10);
 
-    // Variables for paddle
+    // paddles for player 1 and 2; constant paddle size
     const [paddle1Y, setPaddle1Y] = useState(350);
     const [paddle2Y, setPaddle2Y] = useState(350);
     const PADDLE_HEIGHT = 100;
     const PADDLE_THICKNESS = 10;
 
-    let player1Score = 0;
-    let player2Score = 0;
+    // player scoring; winning score is 3
+    const [player1Score, setPlayer1Score] = useState(0);
+    const [player2Score, setPlayer2Score] = useState(0);
     const WINNING_SCORE = 3;
     let winner = false;
 
+    // function for ball movement
     const ballMovement = () => {
         if(winner) return;
+        // sets ball velocity
         setBallX(x => x += ballSpeedX * 0.75);
         setBallY(y => y += ballSpeedY * 0.75);
 
-        if(ballX <= 10) {
-            if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
+        // when ball reaches left side of screen
+        if(ballX <= 20) {
+            if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) { // paddle collision
+                // set ball in reverse direction
                 setBallSpeedX(-ballSpeedX);
                 let deltaY = ballY- (paddle1Y+PADDLE_HEIGHT/2);
                 setBallSpeedY(deltaY * 0.35);
             } 
-            else {
-                player2Score++; // must be before ballReset()
+            else { // wall collision
+                setPlayer2Score(p2score => p2score += 1); // must be before ballReset()
                 ballReset();
             }
         }
-        if(ballX >= 990) {
-            if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+        // when ball reaches right side of screen
+        if(ballX >= 980) {
+            if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) { // paddle collision
+                // set ball in reverse direction
                 setBallSpeedX(-ballSpeedX);
                 let deltaY = ballY - (paddle2Y+PADDLE_HEIGHT/2);
                 setBallSpeedY(deltaY * 0.35);
             }
-            else {
-                player1Score++; 
+            else { // wall collision
+                setPlayer1Score(p1score => p1score += 1); 
                 ballReset();
             }
         }
+        // when ball reaches top or bottom of screen, reverse direction
         if(ballY + ballSpeedY < 10 || ballY + ballSpeedY > 590) {
             setBallSpeedY(-ballSpeedY); 
         }
     };
-    function ballReset() {
-        if(player1Score >= WINNING_SCORE || player2Score >= WINNING_SCORE) 
-            winner = true;
+    // when wall collision occurs
+    const ballReset = () => {
+        // check for winner
+        if(player1Score >= WINNING_SCORE || player2Score >= WINNING_SCORE) winner = true;
         setBallX(500);
         setBallY(300);
         setBallSpeedX(-ballSpeedX);        
     }
 
     //moves based on ball position
-    function computerMovement() {
+    const computerMovement = () => {
         let paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);     
-            if(paddle2YCenter < ballY - 50) setPaddle2Y(paddle2Y+10);
-            else if (paddle2YCenter > ballY + 50) setPaddle2Y(paddle2Y-10);
+            if(paddle2YCenter < ballY - 75) setPaddle2Y(paddle2Y+10);
+            else if (paddle2YCenter > ballY + 75) setPaddle2Y(paddle2Y-10);
     }
 
     //moves based on keyboard input
-    function playerMovement() {
+    const playerMovement = () => {
         let paddle1YCenter = paddle1Y + (PADDLE_HEIGHT/2);     
             if(paddle1YCenter < ballY - 50) setPaddle1Y(paddle1Y+10);
             else if (paddle1YCenter > ballY + 50) setPaddle1Y(paddle1Y-10);
     }
 
+    // used to animate canvas and sets frame counter
     useLayoutEffect(() => {
         let timerId;
         const animate = () => {                
@@ -91,12 +106,15 @@ const Gameplay = () => {
         return () => cancelAnimationFrame(timerId)
     }, [])
 
+    // draw canvas
     useEffect(() => {
+        // gets canvas and context to use
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         context.canvas.height = window.innerHeight;
         context.canvas.width = window.innerWidth;
 
+        // creates green table
         context.fillStyle = '#00A650';
         context.fillRect(TABLE_WIDTH, TABLE_HEIGHT, 1000, 600);
 
@@ -105,15 +123,16 @@ const Gameplay = () => {
         for(let i = TABLE_HEIGHT; i < window.innerHeight - TABLE_HEIGHT; i+=30){
             context.fillRect(TABLE_WIDTH+500, i, 2, 25);
         } 
-        // this is left player paddle
+        // left player paddle
         context.fillRect(TABLE_WIDTH, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT);
-        // this is right computer paddle
+        // right CPU paddle
         context.fillRect(window.innerWidth-TABLE_WIDTH-PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT);
         // draws ball
         context.beginPath();
         context.arc(TABLE_WIDTH+ballX, TABLE_HEIGHT+ballY, 10, 0, Math.PI*2, true);
         context.fill();
         
+        // movement functions
         ballMovement();
         computerMovement();
         playerMovement();
@@ -122,7 +141,6 @@ const Gameplay = () => {
 
     return (
     <div className="container">  
-
       {/* back button */}
       <div className="back">
           <Link to={`../GameplayMenu`}><button className="button">Quit</button></Link>
