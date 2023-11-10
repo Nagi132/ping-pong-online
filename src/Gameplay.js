2ea3b6af9b8c464c04abe9d94bcef02e392dcc02
Savannah Chhann <schhann327@gmail.com>
2023-11-10 11:08:02 -0500

    canvas fixes

--- src/Gameplay.js
+++ src/Gameplay.js
@@ -12,9 +12,9 @@ const Gameplay = () => {
     // frame counter
     const [counter, setCounter] = useState(0);
 
-    // table width and height relative to window dimensions
-    const START_WIDTH = window.innerWidth/2 - 500;
-    const START_HEIGHT = window.innerHeight/2 - 300;
+    // table width and height
+    // const START_WIDTH = window.innerWidth/2 - 500;
+    // const START_HEIGHT = window.innerHeight/2 - 300;
     const TABLE_WIDTH = 1000;
     const TABLE_HEIGHT = 600
 
@@ -37,7 +37,7 @@ const Gameplay = () => {
     const [player1Score, setPlayer1Score] = useState(0);
     const [player2Score, setPlayer2Score] = useState(0);
     const [winner, setWinner] = useState(false);
-    const WINNING_SCORE = 2;
+    const WINNING_SCORE = 5;
 
     // function for ball movement
     const ballMovement = () => {
@@ -47,27 +47,28 @@ const Gameplay = () => {
     };
     const collision = () => { 
         // when ball reaches top or bottom of screen, reverse direction
-        if(ballY + ballSpeedY < -100 || ballY + ballSpeedY > TABLE_HEIGHT-120) {
+        if(ballY + ballSpeedY < 0 || ballY + ballSpeedY > TABLE_HEIGHT) {
             setBallSpeedY(-ballSpeedY); 
         }
 
-        if (ballX < 20 && ballY + 110 > paddle1Y && ballY < paddle1Y) {
+        if (ballX < 0 && ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
             setBallX(x => x + 20);
-            setBallSpeedX(-ballSpeedX);
+            setBallSpeedX(speedX => speedX * -1);
             let deltaY = ballY - (paddle1Y+PADDLE_HEIGHT/2);
-            setBallSpeedY(y => deltaY * -.05);
+            setBallSpeedY(deltaY * (Math.random() < 0.5 ? -.5 : .5));
+ 
         }
-        else if (ballX <= 10){
+        else if (ballX <= 0){
             setPlayer2Score(s => s + 1);
             ballReset();
         }
-        if (ballX > TABLE_WIDTH-10 && ballY + 110 > paddle2Y && ballY < paddle2Y) {
+        if (ballX > TABLE_WIDTH && ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
             setBallX(x => x - 20);
-            setBallSpeedX(-ballSpeedX);
+            setBallSpeedX(speedX => speedX * -1);
             let deltaY = ballY - (paddle2Y+PADDLE_HEIGHT/2);
-            setBallSpeedY(y => deltaY * .05);
+            setBallSpeedY(deltaY * (Math.random() < 0.5 ? -.5 : .5));
         }
-        else if(ballX >= TABLE_WIDTH-10){
+        else if(ballX >= TABLE_WIDTH){
             setPlayer1Score(s => s + 1);
             ballReset();
         } 
@@ -121,24 +122,24 @@ const Gameplay = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         context.canvas.height = TABLE_HEIGHT;
-        context.canvas.width = window.innerWidth;
+        context.canvas.width = TABLE_WIDTH;
 
         // creates green table
         context.fillStyle = '#00A650';
-        context.fillRect(START_WIDTH, 0, TABLE_WIDTH, TABLE_HEIGHT);
+        context.fillRect(0, 0, TABLE_WIDTH, TABLE_HEIGHT);
 
         context.fillStyle = '#FFFFFF';
         // net
-        for(let i = 0; i < window.innerHeight - 225; i+=30){
-            context.fillRect(START_WIDTH+500, i, 2, 25);
+        for(let i = 0; i < TABLE_WIDTH; i+=30){
+            context.fillRect(TABLE_WIDTH/2, i, 2, 25);
         } 
         // left player paddle
-        context.fillRect(START_WIDTH, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT);
+        context.fillRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT);
         // right CPU paddle
-        context.fillRect(window.innerWidth-START_WIDTH-PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT);
+        context.fillRect(TABLE_WIDTH-PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT);
         // draws ball
         context.beginPath();
-        context.arc(START_WIDTH+ballX, START_HEIGHT+ballY, 10, 0, Math.PI*2, true);
+        context.arc(ballX, ballY, 10, 0, Math.PI*2, true);
         context.fill();
 
         // movement functions
@@ -171,7 +172,9 @@ const Gameplay = () => {
         <h1>{player2Score}</h1> 
       </div>
       {/* gameplay */}
-      <canvas ref={canvasRef}/>
+      <div id="canvas-container">
+        <canvas ref={canvasRef}/>
+      </div>
     </div>
     );
 }
